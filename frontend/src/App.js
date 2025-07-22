576c4329630e00dc232901dae2179127c1b1b19e
Raj Jayswal <rajjayswal480@gmail.com>
2025-07-22 20:06:22 +0530

    Minor path change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,14 @@ const App = () => {
      <ToastContainer position="top-center" autoClose={1500} hideProgressBar={true}  />
     <Router>
       <nav className="p-4 bg-gray-800 text-white flex gap-4">
-        <Link to="/add-vehicle">Add Vehicle</Link>
+        <Link to="/">Add Vehicle</Link>
         <Link to="/search-book">Search & Book</Link>
         <Link to="/view-bookings">View Bookings</Link>
 
       </nav>
 
       <Routes>
-        <Route path="/add-vehicle" element={<AddVehiclePage />} />
+        <Route path="/" element={<AddVehiclePage />} />
         <Route path="/search-book" element={<SearchAndBookPage />} />
         <Route path="/view-bookings" element={<BookingsPage />} />
 
